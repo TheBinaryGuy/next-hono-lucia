834652c975d33b5c2729273eba96902ffd5969b5
TheBinaryGuy <hello@thebinary.dev>
2024-05-13 13:48:58 +0530

    upgraded deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,8 @@ import { serverEnvs } from '@/env/server';
 export default {
     schema: './src/services/db/schema.ts',
     out: './src/services/db',
-    driver: 'pg',
+    dialect: 'postgresql',
     dbCredentials: {
-        connectionString: serverEnvs.DATABASE_URL,
+        url: serverEnvs.DATABASE_URL,
     },
 } satisfies Config;
