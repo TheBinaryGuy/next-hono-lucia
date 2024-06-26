a3b97de5552bc414fe06f4b16499835942c15f22
TheBinaryGuy <hello@thebinary.dev>
2024-06-26 22:48:50 +0530

    added login / signup buttons on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+
 import { ThemeToggle } from '@/components/theme-toggle';
 import { Button } from '@/components/ui/button';
+import { Routes } from '@/lib/routes';
 import { getUser } from '@/lib/utils.server';
 import { logout } from '@/server/actions';
 
@@ -7,9 +10,17 @@ export default async function Page() {
     const user = await getUser();
     if (!user) {
         return (
-            <div className='container flex h-full flex-col items-center justify-center gap-2'>
+            <div className='container flex h-full flex-col items-center justify-center gap-4'>
                 <h1 className='text-4xl font-semibold'>Hono x Lucia</h1>
                 <ThemeToggle />
+                <div className='flex items-center gap-4'>
+                    <Button asChild className='w-28'>
+                        <Link href={Routes.login()}>Login</Link>
+                    </Button>
+                    <Button asChild className='w-28' variant='outline'>
+                        <Link href={Routes.getStarted()}>Get Started</Link>
+                    </Button>
+                </div>
             </div>
         );
     }
