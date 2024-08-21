28eecbc53acd18f293e69c72f616493105e0b0ec
TheBinaryGuy <hello@thebinary.dev>
2024-08-21 23:55:53 +0530

    fixed formatting

--- src/app/client-user.tsx
+++ src/app/client-user.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-import { useAuth } from "@/components/auth-provider";
+import { useAuth } from '@/components/auth-provider';
 
 export function ClientUser() {
-    const {user} = useAuth();
-    return (
-        <div>
-            Signed in as: {user.email}
-        </div>
-    );
+    const { user } = useAuth();
+    return <div>Signed in as: {user.email}</div>;
 }
